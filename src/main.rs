cf7958d49e0250bbd9495846f3f59aa3a1da2752
Harsh Modasia <modasis@fzi.de>
2024-03-13 18:05:25 +0100

    first serious commit

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,138 @@
 
 use macroquad::prelude::*;
 
-const SPACING: f32 = 40.0;
+const SPACING: f32 = 20.0;
+const LINE_THICKNESS: f32 = 2.0;
 
+trait Drawable {
+    fn draw(&self);
+}
+enum Orientation {
+    Left,
+    Right,
+    Up,
+    Down,
+}
 struct Rectangle {
-    x1: f32,
-    y1: f32,
-    x2: f32,
-    y2: f32,
+    x: i32,
+    y: i32,
+    w: i32,
+    h: i32,
     color: Color,
 }
 
-impl Rectangle {
-    fn draw_box(&self) {
+struct Ground {
+    x: i32,
+    y: i32,
+    len: i32,
+    ornt: Orientation,
+    color: Color,
+}
+
+impl Drawable for Rectangle {
+    fn draw(&self) {
         draw_rectangle(
-            self.x1,
-            self.y1,
-            self.x2 - self.x1,
-            self.y2 - self.y1,
+            self.x as f32 * SPACING,
+            self.y as f32 * SPACING,
+            self.w as f32 * SPACING,
+            self.h as f32 * SPACING,
             self.color,
         );
     }
 }
-#[macroquad::main("BasicShapes")]
-async fn main() {
-    loop {
-        clear_background(BLUE);
 
-        let obj = Rectangle {
-            x1: 40.0,
-            y1: 40.0,
-            x2: 40.0 + SPACING,
-            y2: 40.0 + SPACING,
-            color: WHITE,
-        };
+impl Drawable for Ground {
+    fn draw(&self) {
+        match self.ornt {
+            Orientation::Up => {
+                draw_line(
+                    self.x as f32 * SPACING,
+                    self.y as f32 * SPACING,
+                    (self.x + self.len) as f32 * SPACING,
+                    self.y as f32 * SPACING,
+                    LINE_THICKNESS,
+                    self.color,
+                );
 
-        draw_2d_grid(SPACING, 1.0, LIGHTGRAY);
+                for i in 0..=(self.len * 2) {
+                    draw_line(
+                        (self.x as f32 + i as f32 * 0.5) * SPACING,
+                        self.y as f32 * SPACING,
+                        ((self.x as f32 + i as f32 * 0.5) - 0.5) * SPACING,
+                        (self.y as f32 + 0.5) * SPACING,
+                        LINE_THICKNESS * 0.5,
+                        self.color,
+                    );
+                }
+            }
 
-        obj.draw_box();
-        // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 20.0, 20.0, WHITE);
+            Orientation::Down => {
+                draw_line(
+                    self.x as f32 * SPACING,
+                    self.y as f32 * SPACING,
+                    (self.x + self.len) as f32 * SPACING,
+                    self.y as f32 * SPACING,
+                    LINE_THICKNESS,
+                    self.color,
+                );
 
-        next_frame().await
+                for i in 0..=(self.len * 2) {
+                    draw_line(
+                        (self.x as f32 + i as f32 * 0.5) * SPACING,
+                        self.y as f32 * SPACING,
+                        ((self.x as f32 + i as f32 * 0.5) + 0.5) * SPACING,
+                        (self.y as f32 - 0.5) * SPACING,
+                        LINE_THICKNESS * 0.5,
+                        self.color,
+                    );
+                }
+            }
+
+            Orientation::Right => {
+                draw_line(
+                    self.x as f32 * SPACING,
+                    self.y as f32 * SPACING,
+                    self.x as f32 * SPACING,
+                    (self.y + self.len) as f32 * SPACING,
+                    LINE_THICKNESS,
+                    self.color,
+                );
+
+                for i in 0..=(self.len * 2) {
+                    draw_line(
+                        self.x as f32 * SPACING,
+                        (self.y as f32 + i as f32 * 0.5) * SPACING,
+                        (self.x as f32 - 0.5) * SPACING,
+                        ((self.y as f32 + i as f32 * 0.5) + 0.5) * SPACING,
+                        LINE_THICKNESS * 0.5,
+                        self.color,
+                    );
+                }
+            }
+
+            Orientation::Left => {
+                draw_line(
+                    self.x as f32 * SPACING,
+                    self.y as f32 * SPACING,
+                    self.x as f32 * SPACING,
+                    (self.y + self.len) as f32 * SPACING,
+                    LINE_THICKNESS,
+                    self.color,
+                );
+
+                for i in 0..=(self.len * 2) {
+                    draw_line(
+                        self.x as f32 * SPACING,
+                        (self.y as f32 + i as f32 * 0.5) * SPACING,
+                        (self.x as f32 + 0.5) * SPACING,
+                        ((self.y as f32 + i as f32 * 0.5) - 0.5) * SPACING,
+                        LINE_THICKNESS * 0.5,
+                        self.color,
+                    );
+                }
+            }
+            _ => todo!(),
+        }
     }
 }
 
@@ -75,5 +164,69 @@ fn draw_2d_grid(spacing: f32, thickness: f32, color: Color) {
             thickness,
             color,
         );
+    }
+}
+
+fn draw_all_objs(vec: Vec<Box<dyn Drawable>>) {
+    for obj in vec {
+        obj.draw();
+    }
+}
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    loop {
+        let blueprint_clr: Color = Color::from_hex(0x2C93FC);
+        clear_background(blueprint_clr);
+        let mut drawable_objects: Vec<Box<dyn Drawable>> = vec![];
+
+        let rec_obj = Rectangle {
+            x: 3,
+            y: 3,
+            w: 6,
+            h: 3,
+            color: WHITE,
+        };
+        drawable_objects.push(Box::new(rec_obj));
+
+        let gnd_obj = Ground {
+            x: 12,
+            y: 3,
+            len: 1,
+            ornt: Orientation::Up,
+            color: WHITE,
+        };
+        drawable_objects.push(Box::new(gnd_obj));
+
+        drawable_objects.push(Box::new(Ground {
+            x: 12,
+            y: 6,
+            len: 3,
+            ornt: Orientation::Down,
+            color: WHITE,
+        }));
+        drawable_objects.push(Box::new(Ground {
+            x: 12,
+            y: 9,
+            len: 5,
+            ornt: Orientation::Right,
+            color: WHITE,
+        }));
+        drawable_objects.push(Box::new(Ground {
+            x: 15,
+            y: 9,
+            len: 5,
+            ornt: Orientation::Left,
+            color: WHITE,
+        }));
+
+        draw_2d_grid(SPACING, 1., LIGHTGRAY);
+
+        draw_all_objs(drawable_objects);
+
+        // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_text("HELLO", 20.0, 20.0, 20.0, WHITE);
+
+        next_frame().await
     }
 }
